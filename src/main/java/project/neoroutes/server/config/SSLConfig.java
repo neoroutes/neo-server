077af71093c4aad7fb2543dcc01d4c2fca6dd55b
mrsgoldenretriever <mrsgoldenretriever@protonmail.com>
2020-02-14 12:06:49 +0330

    Temporary SSL config for test

---
+++ src/main/java/project/neoroutes/server/config/SSLConfig.java
@@ -0,0 +1,48 @@
+package project.neoroutes.server.config;
+
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
+import org.springframework.context.annotation.Configuration;
+import project.neoroutes.server.ServerApplication;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+@Configuration
+public class SSLConfig {
+    private final KeyStore keyStore;
+    private final String password = ServerApplication.password;
+
+    public SSLConfig(KeyStore keyStore) {
+        this.keyStore = keyStore;
+    }
+
+
+    private SSLContext sslContext() throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        return new SSLContextBuilder()
+                .loadTrustMaterial(
+                        keyStore
+                        , new TrustStrategy() {
+                            @Override
+                            public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                                System.out.println("==========");
+                                for (X509Certificate x509Certificate : x509Certificates) {
+                                    System.out.println(x509Certificate.getIssuerX500Principal().toString());
+                                    System.out.println(x509Certificate.getIssuerAlternativeNames() != null ? x509Certificate.getIssuerAlternativeNames().toString() : "");
+                                    System.out.println(new String(x509Certificate.getPublicKey().toString()));
+                                }
+                                System.out.println("==========");
+                                System.out.println(s);
+                                System.out.println("==========");
+                                return true;
+                            }
+                        }
+                ).build();
+    }
+}
